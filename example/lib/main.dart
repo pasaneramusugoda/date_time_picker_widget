e07d9310dee7e4b7e642b8f12ba36c734568431c
Pasan Eramusugoda <vaprera91@gmail.com>
2020-11-17 16:58:34 +0530

    update

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -55,12 +55,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Theme(
       data: Theme.of(context).copyWith(
         primaryColor: _color,
@@ -68,8 +62,6 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       child: Scaffold(
         appBar: AppBar(
-          // Here we take the value from the MyHomePage object that was created by
-          // the App.build method, and use it to set our appbar title.
           title: Text(widget.title),
         ),
         body: SafeArea(
