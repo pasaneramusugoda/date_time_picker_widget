eb6f9aaa676d837b4d3b53867106d59435814bbb
Pasan Eramusugoda <vaprera91@gmail.com>
2020-11-17 14:04:22 +0530

    ui bug fix
    - when theme colors changes all the date highlights didn't changed

--- lib/src/date/date_week_view.dart
+++ lib/src/date/date_week_view.dart
@@ -1,11 +1,14 @@
 import 'package:date_time_picker_widget/src/date_time_picker_view_model.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_screenutil/size_extension.dart';
 import 'package:stacked/stacked.dart';
 
 class DateWeekView extends ViewModelWidget<DateTimePickerViewModel> {
   @override
   Widget build(BuildContext context, DateTimePickerViewModel model) {
+    var w = ((ScreenUtil().screenWidth - 20.w) - (32.w * 7)) / 7;
+    w = (w + w / 7).roundToDouble() + 0.3.w;
     return Container(
       height: 53.0.h,
       child: PageView.builder(
@@ -14,47 +17,54 @@ class DateWeekView extends ViewModelWidget<DateTimePickerViewModel> {
         itemBuilder: (BuildContext context, int index) {
           return Container(
             padding: EdgeInsets.symmetric(horizontal: 10.0.w),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: model.dateSlots[index].days
-                  .map((e) => Center(
-                        child: InkWell(
-                          onTap: !e.enabled
-                              ? null
-                              : () => model.onTapDate(index, e.index),
-                          child: Container(
-                            padding: const EdgeInsets.all(4),
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(90.0),
-                              border: Border.all(
-                                color: e.index == model.selectedDateIndex
-                                    ? Theme.of(context).accentColor
-                                    : Colors.grey,
-                              ),
-                              color: e.enabled
-                                  ? e.index == model.selectedDateIndex
-                                      ? Theme.of(context).accentColor
-                                      : Colors.white
-                                  : Colors.grey.shade300,
-                            ),
-                            alignment: Alignment.center,
-                            width: 32.0.w,
-                            height: 32.0.h,
-                            child: Text(
-                              '${e.date.day}',
-                              style: TextStyle(
-                                  fontSize: 14.0.ssp,
-                                  fontWeight: FontWeight.w500,
-                                  color: e.index == model.selectedDateIndex
-                                      ? Colors.white
-                                      : Colors.grey),
-                              textAlign: TextAlign.center,
-                            ),
+            child: Container(
+              child: ListView.separated(
+                separatorBuilder: (context, index) {
+                  return SizedBox(width: w);
+                },
+                scrollDirection: Axis.horizontal,
+                physics: NeverScrollableScrollPhysics(),
+                itemCount: model.dateSlots[index].days.length,
+                itemBuilder: (context, i) {
+                  var e = model.dateSlots[index].days[i];
+
+                  return Center(
+                    child: InkWell(
+                      onTap: !e.enabled
+                          ? null
+                          : () => model.onTapDate(index, e.index),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(90.0),
+                          border: Border.all(
+                            color: e.index == model.selectedDateIndex
+                                ? Theme.of(context).accentColor
+                                : Colors.grey,
                           ),
+                          color: e.enabled
+                              ? e.index == model.selectedDateIndex
+                                  ? Theme.of(context).accentColor
+                                  : Colors.white
+                              : Colors.grey.shade300,
                         ),
-                      ))
-                  .toList(),
+                        alignment: Alignment.center,
+                        width: 32.0.w,
+                        height: 32.0.w,
+                        child: Text(
+                          '${e.date.day}',
+                          style: TextStyle(
+                              fontSize: 14.0.ssp,
+                              fontWeight: FontWeight.w500,
+                              color: e.index == model.selectedDateIndex
+                                  ? Colors.white
+                                  : Colors.grey),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
             ),
           );
         },
--- lib/src/date/date_weekdays_view.dart
+++ lib/src/date/date_weekdays_view.dart
@@ -8,25 +8,28 @@ class DateWeekdaysView extends ViewModelWidget<DateTimePickerViewModel> {
   Widget build(BuildContext context, DateTimePickerViewModel model) {
     return Container(
       padding: EdgeInsets.symmetric(horizontal: 10.0.w),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: model.weekdays
-            .map((e) => Container(
-                  width: 32.0.w,
-                  child: Text(
-                    '${e['text']}',
-                    style: TextStyle(
-                      fontSize: 18.0.ssp,
-                      color: e['value'] == model.selectedWeekday
-                          ? Colors.black
-                          : Colors.grey,
-                      fontWeight: FontWeight.w600,
+      child: Container(
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: model.weekdays
+              .map((e) => Container(
+                    width: 32.0.w,
+                    // color: Colors.primaries[Random().nextInt(Colors.primaries.length)],
+                    child: Text(
+                      '${e['text']}',
+                      style: TextStyle(
+                        fontSize: 18.0.ssp,
+                        color: e['value'] == model.selectedWeekday
+                            ? Colors.black
+                            : Colors.grey,
+                        fontWeight: FontWeight.w600,
+                      ),
+                      textAlign: TextAlign.center,
                     ),
-                    textAlign: TextAlign.center,
-                  ),
-                ))
-            .toList(),
+                  ))
+              .toList(),
+        ),
       ),
     );
   }
